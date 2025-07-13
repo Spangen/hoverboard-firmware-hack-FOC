4d6f3b4788611f588840d133a39acaf303406d45
Spangen <4445078+Spangen@users.noreply.github.com>
2025-07-13 12:39:59 +0200

    Ser formating

--- Src/main.c
+++ Src/main.c
@@ -493,15 +493,18 @@ int main(void) {
         #if defined(DEBUG_SERIAL_PROTOCOL)
           process_debug();
         #else
+        // Feedback.speedR_meas	  = (int16_t)rtY_Right.n_mot;
+        // Feedback.speedL_meas	  = (int16_t)rtY_Left.n_mot;
           // printf("in1:%i in2:%i cmdL:%i cmdR:%i BatADC:%i BatV:%i TempADC:%i Temp:%i \r\n",
-          printf("in1:%5i in2:%5i cmdL:%5i cmdR:%5i Amp: %5i %5i %5i BatADC:%i BatV:%i TempADC:%i Temp:%i \r\n",
+          printf("1:%5i 2:%5i L:%5i R:%5i Spd:%4i A: %5i %5i %5i BatADC:%i BatV:%i TempADC:%i Temp:%i \r\n",
             input1[inIdx].raw,        // 1: INPUT1
             input2[inIdx].raw,        // 2: INPUT2
             cmdL,                     // 3: output command: [-1000, 1000]
             cmdR,                     // 4: output command: [-1000, 1000]
+            speedAvgAbs,          // 8: Average measured speed in absolute
             dc_curr,                  // 5: Total DC Link Current * 100
-            left_dc_curr,            // 6: Left DC Link Current * 100
-            right_dc_curr,           // 7: Right DC Link Current * 100
+            left_dc_curr,             // 6: Left DC Link Current * 100
+            right_dc_curr,            // 7: Right DC Link Current * 100
             adc_buffer.batt1,         // 5: for battery voltage calibration
             batVoltageCalib,          // 6: for verifying battery voltage calibration
             board_temp_adcFilt,       // 7: for board temperature calibration
